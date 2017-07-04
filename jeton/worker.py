5cf142e918d612b99cd215356f902efd718a13a3
Can Gencer <cgencer@gmail.com>
2017-07-04 18:26:33 +0300

    worker

---
+++ jeton/worker.py
@@ -0,0 +1,24 @@
+import sys
+import select
+
+from hazelcast.serialization.service import SerializationServiceV1
+from hazelcast.config import SerializationConfig, INTEGER_TYPE
+
+config = SerializationConfig()
+service = SerializationServiceV1(config)
+
+rlist = [sys.stdin]
+wlist = [sys.stdout]
+
+def main_loop():
+    sys.stderr.write("Ready to read some bytes\n")
+    while True:
+        ready = select.select(rlist, [], [])[0]
+        for f in ready:
+            bytes = f.read()
+            sys.stderr.write("Read %d bytes\n" % (len(bytes)))
+
+if __name__ == "__main__":
+    main_loop()
+
+
